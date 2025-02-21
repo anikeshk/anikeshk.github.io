24f8311012a31d4b538194c5cbf21782dbee9b27
anikeshk <anikesh.kamath@gmail.com>
2025-02-21 14:32:16 -0500

    Add Google Analytics tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
+import { partytown } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anikeshk.com",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
 });
